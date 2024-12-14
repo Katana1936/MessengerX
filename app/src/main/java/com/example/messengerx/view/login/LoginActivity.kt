3821341787adfff9488e74f58b6a5843f01db3d7
Stas Koloncev <stas.koloncev232@gmail.com>
2024-12-14 23:47:47 +0100

    restart

--- app/src/main/java/com/example/messengerx/view/login/LoginActivity.kt
+++ app/src/main/java/com/example/messengerx/view/login/LoginActivity.kt
@@ -62,7 +62,7 @@ class LoginActivity : ComponentActivity() {
                 if (task.isSuccessful) {
                     val token = auth.currentUser?.uid ?: ""
                     lifecycleScope.launch {
-                        tokenDataStoreManager.saveToken(token) // Сохраняем токен
+                        tokenDataStoreManager.saveToken(token) 
                         navigateToMain()
                     }
                 } else {
